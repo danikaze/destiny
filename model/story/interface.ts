12c4e5de615535f6620cb9eec8602407c76f6851
danikaze <danikaze@gmail.com>
2021-08-12 16:21:34 +0900

    Add Story.state for drafting stories

--- model/story/interface.ts
+++ model/story/interface.ts
@@ -1,8 +1,14 @@
 import { Id, TimestampData } from '@model';
 import { User } from '@model/user';
 
+export enum StoryState {
+  PUBLISHED = 'published',
+  DRAFT = 'draft',
+}
+
 export interface Story extends TimestampData {
   storyId: Id;
+  state: StoryState;
   title: string;
   entryPageId: StoryPage['pageId'];
   authorUserId: User['userId'];
