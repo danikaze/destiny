c29135c9a9e9041f073c4d9d0ef692b9e70b0efe
danikaze <danikaze@gmail.com>
2021-08-12 16:31:57 +0900

    Display own stories in user page

---
+++ components/user-stories/hooks.ts
@@ -0,0 +1,52 @@
+import { callDeleteStoryApi } from '@api/user-story/client';
+import { Story } from '@model/story/interface';
+import { useTranslation } from 'next-i18next';
+import { useState } from 'react';
+import { Props } from '.';
+
+interface State {
+  stories: Story[];
+}
+
+export function useUserStories(props: Props) {
+  const { t } = useTranslation('user-stories');
+  const [state, setState] = useState<State>({ stories: props.stories });
+
+  async function deleteStory(storyId: Story['storyId']) {
+    const currentStory = state.stories.find(
+      (story) => story.storyId === storyId
+    );
+    if (!currentStory) return;
+
+    const deleteIt = confirm(t('confirmDelete', { title: currentStory.title }));
+    if (!deleteIt) return;
+
+    try {
+      await callDeleteStoryApi(storyId);
+    } catch (error) {
+      console.error(error);
+      return;
+    }
+
+    setState((state) => {
+      const index = state.stories.findIndex(
+        (story) => story.storyId === storyId
+      );
+      if (index === -1) return state;
+
+      const updatedStories = [...state.stories];
+      updatedStories.splice(index, 1);
+
+      return {
+        ...state,
+        stories: updatedStories,
+      };
+    });
+  }
+
+  return {
+    t,
+    deleteStory,
+    stories: state.stories,
+  };
+}
--- page-components/user/index.tsx
+++ page-components/user/index.tsx
@@ -1,17 +1,31 @@
 import { useTranslation } from 'next-i18next';
 import { FC } from 'react';
+import { Story } from '@model/story/interface';
 import { Page } from '@components/page';
+import { Icon } from '@components/icon';
 import { UserAccountInfo } from '@components/user-account-info';
+import { UserStories } from '@components/user-stories';
+import { LinkToUserStory } from '@components/links/link-to-user-story';
 
-export interface Props {}
+import styles from './user.module.scss';
 
-export const UserPage: FC<Props> = ({}) => {
+export interface Props {
+  stories: Story[];
+}
+
+export const UserPage: FC<Props> = ({ stories }) => {
   const { t } = useTranslation('common');
+  const { t: tStories } = useTranslation('user-stories');
   const title = `${PACKAGE_NAME} - ${PACKAGE_VERSION} (${COMMIT_HASH_SHORT})`;
 
   return (
     <Page active="user" title={title} header={t('sectionUser')}>
       <UserAccountInfo />
+      <h3>{tStories('yourStories')}</h3>
+      <LinkToUserStory create={true} className={styles.createStoryButton}>
+        <Icon type="plus" /> {tStories('createStoryButton')}
+      </LinkToUserStory>
+      <UserStories stories={stories} />
     </Page>
   );
 };
